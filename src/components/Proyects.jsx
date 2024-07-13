ec8f2caba2c4b3eb5aacab5aef7b1a3cf07a1bdf
artemFedossv <a.fedossov90@gmail.com>
2024-07-12 23:46:33 -0300

    trabajando sobre las cards del portafolio

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export const Proyects = () => {
   return (
-    <div>Proyectos holaaa</div>
+    <div className='cardProyects'>
+      <img className='cardImg' src="/img/proyecto-rocketRiders.png" alt="" />
+      <div className='cardDescription'>
+        <p className='cardTitle'>RocketRiders</p>
+        <p className='cardSubTitle'>proyecto final del curso en coderHouse</p>
+        <div className='containerSkills' >
+          <img className='cardLogo' src="/img/css.png" alt="" />
+          <img className='cardLogo' src="/img/js.png" alt="" />
+          <img className='cardLogo' src="/img/react.png" alt="" />
+        </div>
+        <button>web</button>
+        <button>repo</button>
+      </div>
+    </div>
   )
 }
