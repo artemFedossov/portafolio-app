ad0c0d5791edd2f99fdd295cf24f317f52c679f1
artemFedossv <a.fedossov90@gmail.com>
2024-07-14 23:47:20 -0300

    trabajando en contactos

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,27 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Contacts = () => {
+
+  const [modal, setModal] = useState(false);
+
+  const openModal = () => {
+    setModal(true);
+  }
+
   return (
     <div className='containerContacts'>
       <p className='titleContacts'>contactate conmigo</p>
-      
-        <div className='divGit'>
-          <img className='logoGit' src="/img/gitHub-black.png" alt="" />
-          <p className='contactsDescription' >gitHub</p>
+
+      <a target='_blank' className='divGit' href="https://github.com/artemFedossov/">
+        <img className='logoGit' src="/img/gitHub-black.png" alt="" />
+        <p className='contactsDescription' >gitHub</p>
+      </a>
+      <button className='divEmail' onClick={openModal}>
+        <img className='logoGit' src="/img/email.png" alt="" />
+        <p className='contactsDescription' >email</p>
+      </button>
+      <a target='_blank' className='divlinkedin' href='https://ar.linkedin.com/in/artemfedossov'>
+        <img className='logoGit' src="/img/linkedin.png" alt="" />
+        <p className='contactsDescription' >linkedin</p>
+      </a>
+      <a target='_blank' className='divWhatsApp'>
+        <img className='logoGit' src="/img/whatsApp.png" alt="" />
+        <p className='contactsDescription' >whatsapp</p>
+      </a>
+
+      {modal && (
+        <div className='modal'>
+          <p className='titleForm'>mensaje nuevo</p>
+          <form action="/enviar-correo" method='post'>
+            <div className='fromA'>
+              <label className='labelA' htmlFor="email">para: </label>
+              <input className='inputA' type="email" value="a.fedossov90@gmail.com" readOnly/>
+            </div>
+            <div>
+              <label className='labelB' htmlFor="emael">de: </label>
+              <input className='inputB' type="email" required/>
+            </div>
+            <div className='fromC'>
+              <textarea className='textC' name="mensaje" id="mensaje" cols="30" rows="10" placeholder='Deje su Mensaje: ' required></textarea>
+            </div>
+            <button className="btnCards btnForm" type='submit'>enviar</button>
+          </form>
         </div>
-        <div className='divEmail'>
-          <img className='logoGit' src="/img/email.png" alt="" />
-          <p className='contactsDescription' >email</p>
-        </div>
-        <div className='divlinkedin'>
-          <img className='logoGit' src="/img/linkedin.png" alt="" />
-          <p className='contactsDescription' >linkedin</p>
-        </div>
-        <div className='divWhatsApp'>
-          <img className='logoGit' src="/img/whatsApp.png" alt="" />
-          <p className='contactsDescription' >whatsapp</p>
-        </div>
-      
+
+      )}
+
     </div>
   )
 }
